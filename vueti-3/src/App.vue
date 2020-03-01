0b4661cbc1c566d44b1ae2f6ba5d1d5c27e21e83
Kim,InGee <ingee.kim@gmail.com>
2020-03-01 16:57:58 +0900

    Initial UI Layout

---
+++ vueti-3/src/App.vue
@@ -0,0 +1,75 @@
+<template>
+  <v-app id="inspire">
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+      clipped
+    >
+      <v-list dense>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-code-tags</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Source Code</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-help-circle-outline</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Readme</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar
+      app
+      clipped-left
+    >
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <v-toolbar-title>Application</v-toolbar-title>
+    </v-app-bar>
+
+    <v-content>
+      <v-container
+        class="fill-height"
+        fluid
+      >
+        <v-row
+          align="center"
+          justify="center"
+        >
+          <v-col cols="1" style="background-color:red"/>
+          <v-col cols="8">
+            <v-text-field label="Message"></v-text-field>
+          </v-col>
+          <v-col cols="2">
+            <v-btn color="primary">Send</v-btn>
+          </v-col>
+          <v-col cols="1" style="background-color:red"/>
+        </v-row>
+      </v-container>
+    </v-content>
+
+    <v-footer app>
+      <span>&copy; 2019</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    props: {
+      source: String,
+    },
+    data: () => ({
+      drawer: null,
+    }),
+    created () {
+      this.$vuetify.theme.dark = true
+    },
+  }
+</script>
