59ded0aed0d40ba623b418cd153f6c74544a0080
Kim Jongjin <j2kim99@paran.com>
2020-03-04 17:29:38 +0900

    Add logic to UI layout

--- vueti-3/src/App.vue
+++ vueti-3/src/App.vue
@@ -2,7 +2,7 @@
   <v-app id="inspire">
     <v-navigation-drawer v-model="drawer" app clipped>
       <v-list dense>
-        <v-list-item link href="https://github.com/j2kim99/WebFileShare">
+        <v-list-item link href="https://github.com/j2kim99/WebFileShare" target="_blank">
           <v-list-item-action>
             <v-icon>mdi-code-tags</v-icon>
           </v-list-item-action>
@@ -27,15 +27,27 @@
     </v-app-bar>
 
     <v-content>
+      <v-row>
+        <v-col cols="1" />
+          <v-col>
+            <h1>Your code is <span class="blue--text text--lighten-2">{{ peerID }}</span> </h1>
+          </v-col>
+      </v-row>
+      <v-row v-if="conn">
+        <v-col cols="1" />
+          <v-col>
+            <h1> Currently connected to <span class="purple--text text--lighten-2">{{connID}}</span> </h1>
+            </v-col>
+      </v-row>
       <v-row align="center" justify="center" dense>
         <v-col cols="1" />
         <v-col cols="8">
-          <v-text-field label="Reciever's Code"></v-text-field>
+          <v-text-field label="Reciever's Code" v-model="targetID" v-on:keyup.13="join"></v-text-field>
         </v-col>
         <v-col cols="1" align="center">
           <v-tooltip top>
             <template v-slot:activator="{ on }">
-              <v-btn color="blue" dark v-on="on"
+              <v-btn color="blue" dark v-on="on" v-on:click="join"
                 ><v-icon>mdi-phone-forward-outline</v-icon></v-btn
               >
             </template>
@@ -45,7 +57,7 @@
         <v-col cols="1" align="center">
           <v-tooltip top>
             <template v-slot:activator="{ on }">
-              <v-btn color="red" dark v-on="on"
+              <v-btn color="red" dark v-on="on" v-on:click="disconnect"
                 ><v-icon>mdi-phone-off-outline</v-icon></v-btn
               >
             </template>
@@ -57,12 +69,12 @@
       <v-row align="center" justify="center" dense>
         <v-col cols="1" />
         <v-col cols="8">
-          <v-text-field label="Message"></v-text-field>
+          <v-text-field label="Message" v-model="message" v-on:keyup.13="send"></v-text-field>
         </v-col>
         <v-col cols="1" align="center">
           <v-tooltip top>
             <template v-slot:activator="{ on }">
-              <v-btn color="green" dark v-on="on"
+              <v-btn color="green" dark v-on="on" v-on:click="send"
                 ><v-icon>mdi-send</v-icon></v-btn
               >
             </template>
@@ -75,12 +87,12 @@
       <v-row align="center" justify="center" dense>
         <v-col cols="1" />
         <v-col cols="8">
-          <template><v-file-input label="File"></v-file-input></template>
+          <template><v-file-input label="File" id="file"></v-file-input></template>
         </v-col>
         <v-col cols="1" align="center">
           <v-tooltip top>
             <template v-slot:activator="{ on }">
-              <v-btn color="brown" dark v-on="on"
+              <v-btn color="brown" dark v-on="on" v-on:click="sendFile"
                 ><v-icon>mdi-file-send</v-icon></v-btn
               >
             </template>
@@ -105,24 +117,23 @@
                     mdi-arrow-left
                   </v-icon>
                 </template>
-                <span> {{ q.time }} </span>
+                <span> {{ hms(q) }} </span>
               </v-tooltip>
               <v-list-item-content v-if="q.data.type == 'str'">
                 {{ q.data.data }}
               </v-list-item-content>
-              <v-list-item-content v-if="q.data.type == 'file'">
+              <v-list-item-content v-if="q.data.type == 'fileSrc'">
                 {{ q.data.name }}
               </v-list-item-content>
-              <v-tooltip right v-if="q.data.type == 'file'">
+              <v-tooltip right v-if="q.data.type == 'fileSrc'">
                 <template v-slot:activator="{ on }">
                   <v-btn
                     v-on="on"
                     color="yellow"
                     :href="q.data.data"
                     :download="q.data.name"
-                    target="_blank"
                   >
-                    <v-icon v-if="q.data.type == 'file'" color="black">
+                    <v-icon v-if="q.data.type == 'fileSrc'" color="black">
                       mdi-download
                     </v-icon>
                   </v-btn>
@@ -149,36 +160,191 @@ export default {
   },
   data: () => ({
     drawer: null,
-    que: [
-      {
-        src: "send",
-        time: "12:34",
-        data: { type: "str", data: "hello" }
-      },
-      {
-        src: "recv",
-        time: "12:35",
-        data: { type: "str", data: "hello to you too" }
-      },
-      {
-        src: "send",
-        time: "12:36",
-        data: {
-          type: "file",
-          name: "hello.jpg",
-          data:
-            "https://j2kim99.github.io/WebFileShare/fileTutorial/helloworld.jpg"
-        }
-      },
-      {
-        src: "system",
-        time: "12:37",
-        data: { type: "str", data: "Disconnected" }
-      }
-    ]
+    peer: null,
+    conn: null,
+    peerID: "",
+    connID: "",
+    targetID: "",
+    message: "",
+    que: []
   }),
   created() {
+    var vm = this;
+    console.log(vm);
+
     this.$vuetify.theme.dark = true;
+
+    this.peer = new Peer(null, {
+      debug: 2
+    });
+    this.peer.on('open', function(id){
+      console.log('ID: '+id);
+      vm.peerID=id;
+    });
+    this.peer.on('connection', function(c){
+      if(vm.conn){
+        c.on('open', function(){
+          c.send({type: "str", data: "Already Connected"});
+          setTimeout(function(){c.close(); }, 500);
+        });
+        return;
+      }
+
+      vm.conn = c;
+      console.log("Connected: "+vm.conn.peer);
+      var t = new Date();
+      vm.que.push({
+        src: "system",
+        time:t,
+        data: {
+          type: "str",
+          data: "Connected to "+vm.conn.peer
+        }
+      });
+      vm.connID=""+vm.conn.peer;
+      vm.ready();
+    });
+    this.peer.on('disconnected', function(){
+      this.que.push({
+        src: "system",
+        data: {
+          type: "str",
+          data: "Disconnected"
+        }
+      });
+      console.log("Disconnected");
+      vm.connID="";
+    });
+    this.peer.on('close', function(){
+      this.que.push({
+        src: "system",
+        data: {
+          type: "str",
+          data: "Connection Closed"
+        }
+      });
+      console.log("Closed");
+      vm.connID="";
+    });
+    this.peer.on('error', function(err){
+      console.log(err);
+      alert(''+err);
+    });
+  },
+  methods: {
+    ready: function(){
+      var vm = this;
+      this.conn.on('data', function(data){
+        console.log("Data recieved");
+        var t=new Date();
+        vm.que.push({src:"recv", time:t, data:data});
+      });
+    },
+    join: function(){
+      var vm = this;
+      if(this.conn){
+        this.conn.close();
+        this.connID = "";
+        var t = new Date();
+        vm.que.push({
+          src:"system",
+          time:t,
+          data:{
+            type:"str",
+            data:"Disconnected"
+          }
+        });
+      }
+      this.conn=this.peer.connect(this.targetID, {
+        reliable: true
+      });
+      this.targetID="";
+      this.conn.on('open', function(){
+        console.log("Connected to: "+vm.conn.peer);
+        var t = new Date();
+        vm.que.push({
+          src:"system",
+          time:t,
+          data:{
+            type:"str",
+            data:"Connected to "+vm.conn.peer
+          }
+        });
+        vm.connID=""+vm.conn.peer;
+      });
+      this.conn.on('data', function(data){
+        console.log("Data recieved");
+        var t = new Date();
+        vm.que.push({src:"recv", time:t, data:data});
+      });
+    },
+    disconnect: function(){
+      var vm = this;
+      if(this.conn){
+        this.conn.close();
+        var t = new Date();
+        vm.que.push({
+          src:"system",
+          time:t,
+          data:{
+            type:"str",
+            data:"Disconnected from "+this.connID
+          }
+        });
+        this.conn = null;
+        this.connID="";
+      }
+    },
+    clear: function(){
+      var vm = this;
+      var t = new Date();
+      vm.que = [{src:"system", time:t, data:{type:"str", data:"Message Cleared"}}];
+    },
+    send: function(){
+      var vm = this;
+      if(vm.conn && vm.conn.open){
+        var data={
+          type: "str",
+          data: vm.message
+        }
+        var t = new Date();
+        vm.conn.send(data);
+        vm.que.push({src:"send", time:t, data:data});
+        vm.message="";
+      }else{
+        alert("Connection Lost");
+        this.disconnect();
+      }
+    },
+    sendFile: function(){
+      var vm = this;
+      if(!vm.conn || !vm.conn.open){
+        alert("Connection Lost");
+        this.disconnect();
+      }
+      var file = document.getElementById("file").files[0];
+      var reader = new FileReader();
+      reader.onload = (function(e){
+        var data = {
+          type: "fileSrc",
+          name: file.name,
+          data: e.target.result
+        };
+        vm.conn.send(data);
+        var t = new Date();
+        vm.que.push({src:"send", time: t, data:data});
+      });
+      reader.readAsDataURL(file);
+    },
+    hms: function(q){
+      var vm = this;
+      var h=q.time.getHours();
+      var m=q.time.getMinutes();
+      var s=q.time.getSeconds();
+      if(m<10) {m='0'+m};
+      if(s<10) {s='0'+s};
+      var hms = "[" + h + ":" + m + ":" + s + "] ";
+    }
   }
 };
 </script>
--- vueti-3/src/main.js
+++ vueti-3/src/main.js
@@ -2,10 +2,11 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import vuetify from "./plugins/vuetify";
+import Peer from "peerjs";
 
 Vue.config.productionTip = false;
 
-new Vue({
+var vm = new Vue({
   router,
   vuetify,
   render: h => h(App)
